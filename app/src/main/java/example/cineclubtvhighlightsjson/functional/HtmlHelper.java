76ece3749f8c3ea72b8c14521a4077cd0a355ff9
manuel <manuel@letsgo2.net>
2017-07-29 17:32:37 +0200

    HtmlHelper

--- app/src/main/java/example/cineclubtvhighlightsjson/functional/HtmlHelper.java
+++ app/src/main/java/example/cineclubtvhighlightsjson/functional/HtmlHelper.java
@@ -1,8 +1,34 @@
-package example.cineclubtvhighlightsjson.entities;
+package example.cineclubtvhighlightsjson.functional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mlu on 18.06.2017.
  */
 
 class HtmlHelper {
+    private static Map<String,String> HTML_CHARACTERS = new HashMap<>();
+
+    static{
+        HTML_CHARACTERS.put( "&auml;", "ä" );
+        HTML_CHARACTERS.put( "&ouml;", "ö" );
+        HTML_CHARACTERS.put( "&uuml;", "ü" );
+        HTML_CHARACTERS.put( "&Auml;", "Ä" );
+        HTML_CHARACTERS.put( "&Ouml;", "Ö" );
+        HTML_CHARACTERS.put( "&Uuml;", "Ü" );
+        HTML_CHARACTERS.put( "&szlig;", "ß" );
+//        HTML_CHARACTERS.put( "\"", "\"" );
+    }
+
+    public static String replaceHtmlCharacters( String s ){
+        for (Map.Entry<String,String> entry : HTML_CHARACTERS.entrySet() ){
+
+            String htmlCharacterSequence = entry.getKey();
+            String character = entry.getValue();
+            s = s.replace( htmlCharacterSequence, character );
+
+        }
+        return s;
+    }
 }
